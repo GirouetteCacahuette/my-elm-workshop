c8ce27d9b2cf6e2539c3951131ba9a6e3647ab36
Martin Prieur <martin.prieur@outlook.fr>
2019-08-26 11:46:30 +0200

    Step 2

--- Step02/ResultPage.elm
+++ Step02/ResultPage.elm
@@ -1,14 +1,15 @@
 module Step02.ResultPage exposing (main, resultPage)
 
-import Html exposing (Html, div, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, a, div, h1, text)
+import Html.Attributes exposing (class, href)
 import Utils.Utils exposing (styles, testsIframe)
 
 
 resultPage : Int -> Html msg
 resultPage score =
-    div []
-        [ text "Content should go here"
+    div [ class "score" ]
+        [ h1 [] [ text "Your score: ", text (String.fromInt score), text " / 5" ]
+        , a [ class "btn btn-primary", href "#" ] [ text "Replay" ]
         ]
 
 
